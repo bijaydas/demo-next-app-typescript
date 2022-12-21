e64123e637b3912aa674c82685059d5c2ade3d99
bijaydas <imbijaydas@gmail.com>
2022-12-21 20:00:12 +0530

    initial commit :tada:

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import type { NextPage } from 'next';
+
+const IndexPage: NextPage = () => {
+  return (
+    <section>
+      Welcome
+    </section>
+  );
+};
+
+export default IndexPage;
